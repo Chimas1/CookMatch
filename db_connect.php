0e29bcc95345598a073237d8e992a06e9aa88f2b
Chimas1 <chimas@schuldorf.de>
2025-06-03 08:16:31 +0200

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -19,19 +19,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error); 
 }
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
- 
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (PDOException $e) {
-    echo "Datenbankverbindung fehlgeschlagen: " . $e->getMessage();
-    exit;
-}
 
 echo "Verbindung hergestellt";
 
