fc2da9f7715c80a9e3fbbc0fc460bcd79874f5cb
Chimas1 <chimas@schuldorf.de>
2025-06-03 08:34:12 +0200

    Update and rename db_connect.php to Database.php
    Changed to class style

---
+++ Database.php
@@ -0,0 +1,42 @@
+<?php
+class Database {
+$servername; 
+$username; 
+$password; 
+$dbname; 
+
+$conn;
+
+function __construct()
+  {
+    $this->servername = "localhost"; 
+  $this->username = "CookMatch"; 
+  $this->password = "SBScookmatch"; 
+  $this->dbname = "CookMatch"; 
+  }
+
+function connect()
+  {
+    // Conection herstellen
+    $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname); 
+    
+
+    if ($conn->connect_error) { 
+        die("Connection failed: " . $conn->connect_error); 
+    }
+
+    //return $this->conn;
+  }
+
+function query($query_string)
+  {
+    $this->conn->query($query_string);
+  }
+
+function disconnect()
+  {
+    // Conection schließen
+    $conn->close();
+  }
+}
+?>
