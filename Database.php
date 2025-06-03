c0449346ac505668981ee899b20e5dd12448556e
Chimas1 <chimas@schuldorf.de>
2025-06-03 08:39:25 +0200

    Update Database.php
    Added missing this

--- Database.php
+++ Database.php
@@ -35,7 +35,7 @@ function query($query_string)
 
   function preparedStm($query_string, $binder, $value)
   {
-        $stmt = $conn->prepare($query_string);
+        $stmt = $this->conn->prepare($query_string);
         $stmt->bind_param($binder, $value);
         $stmt->execute();
         return $stmt->get_result();
