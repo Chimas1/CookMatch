6d0653f3e080bbd337fe729b35375df4a847cf45
LeaN0208 <leanot1@schuldorf.de>
2025-06-06 10:17:31 +0200

    Update kühlschrank.php

--- kühlschrank.php
+++ kühlschrank.php
@@ -1,14 +1,21 @@
 <?php
 require_once("Database.php");
 
-
-
 ?>
+
+
+
 <html>
   <head>
     
   </head>
   <body>
+    
     <h1>Kühlschrank</h1>
+  <form action="insert.php" method="post">
+    Value1: <input type="text" name = "Lebensmittel" /><br/>
+ <input type="submit" />
+</form>
+    
   </body>
 </html>
