641ac444f4596fb98f61c8d141529bff7f4538a7
Chimas1 <chimas@schuldorf.de>
2025-06-06 14:56:57 +0200

    Update rezepte-erstellen.php
    Hab die Datenbank verbindung hinzugefügt mit der Überprüfung, ob der Nutzer eingeloggt ist.

--- rezepte-erstellen.php
+++ rezepte-erstellen.php
@@ -1,1 +1,10 @@
+<?php
+session_start();
+require_once 'db.php'; // Deine DB-Verbindung
 
+// Prüfen, ob der Benutzer angemeldet ist
+if (!isset($_SESSION['userid'])) {
+    die("Bitte zuerst einloggen.");
+}
+  
+?>
