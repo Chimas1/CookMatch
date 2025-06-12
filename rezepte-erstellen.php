cc85039db16b84e6f43e90b43c36aacaf6b1f8a7
nora006 <norgan@schuldorf.de>
2025-06-12 09:52:28 +0200

    Update rezepte-erstellen.php
    Code verworfen

--- rezepte-erstellen.php
+++ rezepte-erstellen.php
@@ -1,10 +1,1 @@
-<?php
-session_start();
-require_once 'db.php'; // Deine DB-Verbindung
 
-// Prüfen, ob der Benutzer angemeldet ist
-if (!isset($_SESSION['userid'])) {
-    die("Bitte zuerst einloggen.");
-}
-  
-?>
