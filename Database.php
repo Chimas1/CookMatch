ebeb926cdde49c39b22820fa00dc0177a9769d7c
nora006 <norgan@schuldorf.de>
2025-06-12 15:13:14 +0200

    Update Database.php

--- Database.php
+++ Database.php
@@ -47,6 +47,9 @@ function query($query_string)
   //WICHTIG: Wenn ihr SELECT Anweisungen stellt immer nur EINEN Wert übergeben
   function select($query_string, $binder, $value)
   {
+    echo $query_string;
+    echo $binder;
+    echo $value;
     $stmt = $this->preparedStm($query_string, $binder, $value);
     /* execute query */
     $stmt->execute();
