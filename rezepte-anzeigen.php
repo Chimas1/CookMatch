a928365f4fc7ad444a1635e98997e0c9ebd403fe
nora006 <norgan@schuldorf.de>
2025-06-13 11:09:57 +0200

    Update rezepte-anzeigen.php

--- rezepte-anzeigen.php
+++ rezepte-anzeigen.php
@@ -52,7 +52,7 @@ if ($row = $rezept->fetch_assoc()) {
     echo "</ul>";
 
     // Kochutensilien
-    $utensilien = $db->select("SELECT Kochutensilien.Titel FROM Kochutensilien, Braucht, Anweisung, Rezept WHERE Kochutensilien.Titel=Braucht.Titel and Braucht.ID=Anweisung.ID and Anweisung.Name=Rezept.Name and Rezept.Name = ?", "s", $id);
+    $utensilien = $db->select("SELECT Kochutensilien.Titel distinct FROM Kochutensilien, Braucht, Anweisung, Rezept WHERE Kochutensilien.Titel=Braucht.Titel and Braucht.ID=Anweisung.ID and Anweisung.Name=Rezept.Name and Rezept.Name = ?", "s", $id);
     
  echo "<h3>Kochutensilien:</h3><ul>";
     while ($u = $utensilien->fetch_assoc()) {
