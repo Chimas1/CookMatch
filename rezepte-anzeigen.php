844fdbaefd1ab0fe14af76f60ad49adab4fac319
nora006 <norgan@schuldorf.de>
2025-06-13 10:09:24 +0200

    Update rezepte-anzeigen.php

--- rezepte-anzeigen.php
+++ rezepte-anzeigen.php
@@ -63,7 +63,7 @@ if ($row = $rezept->fetch_assoc()) {
  // Anweisungen
    echo "<div class='section-title'>Zubereitung</div>";
     $anweisungen = $db->select("SELECT * FROM Anweisung, Rezept WHERE Anweisung.Name=Rezept.Name and Rezept.Name= ? ORDER BY Anweisung.ID", "s", $id);
-    echo <ol class= '<h3>Anweisungen:</h3><ol>';
+    echo "<ol class= '<h3>Anweisungen:</h3><ol>'";
     while ($a = $anweisungen->fetch_assoc()) {
         echo "<li>" . htmlspecialchars($a['Text']) . "</li>";
     }
