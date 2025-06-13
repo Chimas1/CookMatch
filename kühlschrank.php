adcab701c0a0159cac793d791491450abdbc17fa
LeaN0208 <leanot1@schuldorf.de>
2025-06-13 08:58:30 +0000

    update

--- kühlschrank.php
+++ kühlschrank.php
@@ -5,33 +5,27 @@ require_once("Database.php");
 
       $db = new Database();
       $db->connect();
-
-// Check if the form is submitted
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    
-
-//Hier die versprochene Überprüfung, ob der Nutzer eingeloggt ist, einfach // wegmachen, dann sollte es klappen
+      //Hier die versprochene Überprüfung, ob der Nutzer eingeloggt ist, einfach // wegmachen, dann sollte es klappen
 //session_start();
 //if (!isset($_SESSION['userid'])) {
   //  die("Bitte zuerst einloggen. <br> <a href='login.php>Zum Login'</a>");
 //}
-
-
-// Don't forget to properly escape your values before you send them to DB
-// to prevent SQL injection attacks.
-
-$bezeichnung = $_POST['Bezeichnung'];
 $benutzername = "";
-$anzahl = $_POST['Anzahl'];
+// Check if the form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Don't forget to properly escape your values before you send them to DB
+  // to prevent SQL injection attacks.
+
+  $bezeichnung = $__POST['Bezeichnung'];
+  $anzahl = $__POST['Anzahl'];
 
 
-$query = "INSERT INTO `Besitzt` (`Bezeichnung`, `Benutzername`, `Anzahl`) VALUES (?, ?, ?)";
-$result = $db->insert($query, "sss", [$bezeichnung, $benutzername, $anzahl]);
-
+  $query = "INSERT INTO `Besitzt` (`Bezeichnung`, `Benutzername`, `Anzahl`) VALUES (?, ?, ?)";
+  $result = $db->insert($query, "sss", [$bezeichnung, $benutzername, $anzahl]);
 }
 
-$query = "SELECT * FROM Besitzt";
-$result = $db->query($query);
+$query = "SELECT * FROM Besitzt WHERE Benutzername = ? ORDER BY Bezeichnung ASC";
+$result = $db->select($query, "s", [$benutzername]);
 echo "<b> <center>Database Output</center> </b> <br> <br>";
 
 
@@ -60,7 +54,8 @@ $db->disconnect();
     <h1>Kühlschrank</h1>
     
   <form action="insert.php" method="post">
-    Value 1: <input type="text" name = "Lebensmittel" /><br/>
+    Lebensmittel: <input type="text" name = "Bezeichnung" /><br/>
+    Anzahl: <input type="number" name = "Anzahl" /><br/>
  <input type="submit" />
 </form>
     
