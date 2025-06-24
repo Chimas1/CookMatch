4e2965fe8cd656ee28b1e4dff647774fc0485e46
AnnHet <annhet@schuldorf.de>
2025-06-24 08:36:12 +0200

    Update rezepte-suchen.php
    vorherige Änderung rückgängig

--- rezepte-suchen.php
+++ rezepte-suchen.php
@@ -4,8 +4,8 @@ $db = new Database();
 $conn = $db->connect();
 $rezeptname ="";
 
-$suchbegriff = $conn->real_escape_string($_GET['rezeptname']);
 if (isset($_GET['rezeptname'])) {
+    $suchbegriff = $conn->real_escape_string($_GET['rezeptname']);
   
     $sql = "SELECT name FROM Rezept WHERE name LIKE '%$rezeptname%'";
     $result = $conn->query($sql);
