e61cda315f66d654838670410fef59b684cd30ae
Chimas1 <chimas@schuldorf.de>
2025-06-27 11:18:27 +0200

    Update Nutzersuchen.php

--- Nutzersuchen.php
+++ Nutzersuchen.php
@@ -11,38 +11,3 @@ $db = new Database();
 $db->connect();
     
 ?>
-
-<!DOCTYPE html>
-<html lang="de">
-<head>
-    <meta charset="UTF-8">
-    <title>Nutzer suchen</title>
-   <div style="position:absolute; top:10px; right:10px;">
-</div>
-</head>
-<body
-     style="background-color:#FFA500;">
-  <form action="Nutzersuchen.php" method="get">
-    <input type="text" name="name" placeholder="Rezeptname eingeben">
-    <button type="submit">Suchen</button>
-</form>
-<div class="container">
-<?php
-if (!empty($name)) {
-    // Trefferliste
-    elseif ($suchErgebnisse && $suchErgebnisse->num_rows > 0) {
-        echo '<h2>Suchergebnisse:</h2><ul>';
-        while ($row = $suchErgebnisse->fetch_assoc()) {
-            echo '<li><a href="rezepte-anzeigen.php?name=' . urlencode($row['name']) . '">' . htmlspecialchars($row['name']) . '</a></li>';
-        }
-        echo '</ul>';
-    }
-    // Kein Treffer
-    else {
-        echo '<p>Kein Rezept unter diesem Namen gefunden.</p>';
-    }
-}
-?>
-</div>
-</body>
-</html>
